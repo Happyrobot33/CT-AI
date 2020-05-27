afc422c8edc3e8fe316715d4308a59a001766850
Noah van der Kraan <noahkra@gmail.com>
2020-05-28 00:41:00 +0200

    Added pausebuffer to output

--- output.js
+++ output.js
@@ -1,6 +1,13 @@
 const robot = require('robot.js'); // Keyboard & mouse input is done via this module
 
 class Output {
+	pauseBuffer() {
+		robot.keyTap('escape');
+		setTimeout(() => {
+			robot.keyTap('escape');
+		}, 100);
+	}
+
 	move(w, a, s, d, space) {
 		robot.keyToggle('w', w ? "down" : "up", "shift");
 		robot.keyToggle('a', a ? "down" : "up", "shift");
